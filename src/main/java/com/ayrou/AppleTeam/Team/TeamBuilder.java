9fb5ec3a76c3352b1a7e53b10f06042aa57312b4
Cyanide <slm789456123@gmail.com>
2019-06-30 18:32:29 +0800

    增加隊伍名稱驗證機制

--- src/main/java/com/ayrou/AppleTeam/Team/TeamBuilder.java
+++ src/main/java/com/ayrou/AppleTeam/Team/TeamBuilder.java
@@ -13,6 +13,23 @@ public class TeamBuilder {
     Invite invite;
     ItemGet itemGet;
 
+    public static boolean checkTeamName(String name) {
+
+        int num = 0;
+        boolean matches = true;
+        for (int i = 0,j = name.length(); i < j; i++) {
+            String temp = name.substring(i, i + 1);
+            if (temp.matches("[\u4e00-\u9fa5]")) {
+                num += 1;
+            }
+            else if (temp.matches("[a-zA-Z0-9]*")) {
+                num += 1;
+            }
+            else matches =! matches;
+        }
+        return (num <= 10 & num >= 3) & matches;
+    }
+
     public TeamBuilder setName(String name) {
         if (name == null) throw new NullPointerException();
         this.name = name;
