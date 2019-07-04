2995e0c01b88b3e8ddcb217bd7567c45d49982a4
Cyanide <slm789456123@gmail.com>
2019-07-04 22:50:01 +0800

    增加 隊伍資訊 介面

---
+++ src/main/java/com/ayrou/AppleTeam/GUI/GUIs/TeamInfo.java
@@ -0,0 +1,22 @@
+package com.Ayrou.AppleTeam.GUI.GUIs;
+
+import com.Ayrou.AppleTeam.AppleTeam;
+import com.Ayrou.AppleTeam.GUI.SubGui;
+import com.github.stefvanschie.inventoryframework.Gui;
+import org.bukkit.entity.Player;
+
+public class TeamInfo extends SubGui {
+    private String titleName = "";
+    private Gui gui = new Gui(AppleTeam.getInstance(), 6, titleName);
+
+
+    @Override
+    public void openInventory(Player player) {
+        gui.show(player);
+    }
+
+    @Override
+    public String titleName() {
+        return titleName;
+    }
+}
